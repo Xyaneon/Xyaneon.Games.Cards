0efb5e9676d708688642cb7f65e00f6e3d70e186
Christopher Kyle Horton <christhehorton@gmail.com>
2019-01-29 00:05:30 -0500

    Define shuffling algorithm interface and a default class to use.

---
+++ Xyaneon.Games.Cards/DefaultShuffleAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xyaneon.Games.Cards
+{
+    /// <summary>
+    /// A default shuffling algorithm for cards.
+    /// </summary>
+    /// <typeparam name="TCard">
+    /// The <see cref="Type"/> of cards this algorithm will be used to shuffle.
+    /// </typeparam>
+    public class DefaultShuffleAlgorithm<TCard> : IShuffleAlgorithm<TCard> where TCard : Card
+    {
+        #region IShuffleAlgorithm<TCard> implementation
+
+        /// <summary>
+        /// Shuffles the provided collection of cards and returns them in a new
+        /// <see cref="IList{T}"/> indicating the shuffled order.
+        /// </summary>
+        /// <param name="cards">
+        /// The collection of cards to shuffle.
+        /// </param>
+        /// <returns>
+        /// A new list containing the supplied <paramref name="cards"/>
+        /// in shuffled order.
+        /// </returns>
+        public IList<TCard> Shuffle(IEnumerable<TCard> cards)
+        {
+            var random = new Random();
+            return cards.OrderBy(c => random.Next()).ToList();
+        }
+
+        #endregion // End IShuffleAlgorithm<TCard> implementation region.
+    }
+}
---
+++ Xyaneon.Games.Cards/IShuffleAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xyaneon.Games.Cards
+{
+    /// <summary>
+    /// Interface for classes which can be used for shuffling cards.
+    /// </summary>
+    /// <typeparam name="TCard">
+    /// The <see cref="Type"/> of cards this algorithm will be used to shuffle.
+    /// </typeparam>
+    public interface IShuffleAlgorithm<TCard> where TCard : Card
+    {
+        #region Methods
+
+        /// <summary>
+        /// Shuffles the provided collection of cards and returns them in a new
+        /// <see cref="IList{T}"/> indicating the shuffled order.
+        /// </summary>
+        /// <param name="cards">
+        /// The collection of cards to shuffle.
+        /// </param>
+        /// <returns>
+        /// A new list containing the supplied <paramref name="cards"/>
+        /// in shuffled order.
+        /// </returns>
+        IList<TCard> Shuffle(IEnumerable<TCard> cards);
+
+        #endregion // End methods region.
+    }
+}
