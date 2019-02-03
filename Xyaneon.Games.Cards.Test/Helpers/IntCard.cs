0a884cb8c4e75fc89df7c96841f9a083235b0618
Christopher Kyle Horton <christhehorton@gmail.com>
2019-02-03 13:16:56 -0500

    Create an IntCard helper class for unit testing.

---
+++ Xyaneon.Games.Cards.Test/Helpers/IntCard.cs
@@ -0,0 +1,33 @@
+﻿namespace Xyaneon.Games.Cards.Test.Helpers
+{
+    /// <summary>
+    /// Represents a card with an associated <see langword="int"/> value.
+    /// </summary>
+    public class IntCard : Card
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntCard"/> class with
+        /// the specified <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the card.
+        /// </param>
+        public IntCard(int value)
+        {
+            Value = value;
+        }
+
+        #endregion // End constructors region.
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the value of this <see cref="IntCard"/>.
+        /// </summary>
+        public int Value { get; }
+
+        #endregion // End properties region.
+    }
+}
