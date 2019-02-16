f066b42c76d995ca914af6c5561ecebc49bc7fb2
Christopher Kyle Horton <christhehorton@gmail.com>
2019-02-16 12:19:36 -0500

    Have StandardPlayingCard implement IEquatable<T> (#11).

--- Xyaneon.Games.Cards/StandardPlayingCards/StandardPlayingCard.cs
+++ Xyaneon.Games.Cards/StandardPlayingCards/StandardPlayingCard.cs
@@ -1,4 +1,6 @@
-﻿namespace Xyaneon.Games.Cards.StandardPlayingCards
+﻿using System;
+
+namespace Xyaneon.Games.Cards.StandardPlayingCards
 {
     /// <summary>
     /// A card from a standard 52-card deck.
@@ -10,7 +12,7 @@
     /// <seealso cref="StandardPlayingCards.Rank"/>
     /// <seealso cref="StandardPlayingCards.Suit"/>
     /// <seealso cref="StandardPlayingCardDeck"/>
-    public class StandardPlayingCard : Card
+    public class StandardPlayingCard : Card, IEquatable<StandardPlayingCard>
     {
         #region Constructors
 
@@ -33,6 +35,35 @@
 
         #endregion // End constructors region.
 
+        #region IEquatable<StandardPlayingCard> implementation
+
+        /// <summary>
+        /// Indicates whether the current <see cref="StandardPlayingCard"/>
+        /// is equal to another <see cref="StandardPlayingCard"/>.
+        /// </summary>
+        /// <param name="other">
+        /// The <see cref="StandardPlayingCard"/> to compare with this
+        /// object.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the other
+        /// <see cref="StandardPlayingCard"/> is equal to this
+        /// <see cref="StandardPlayingCard"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        /// <seealso cref="IEquatable{T}.Equals(T)"/>
+        public bool Equals(StandardPlayingCard other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Rank.Equals(other.Rank) && Suit.Equals(other.Suit);
+        }
+
+        #endregion // End IEquatable<StandardPlayingCard> implementation region.
+
         #region Properties
 
         /// <summary>
@@ -46,5 +77,118 @@
         public Suit Suit { get; }
 
         #endregion // End properties region.
+
+        #region Methods
+
+        #region Public methods
+
+        /// <summary>
+        /// Determines whether the specified object is equal to
+        /// this <see cref="StandardPlayingCard"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The object to compare to the current
+        /// <see cref="StandardPlayingCard"/>.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the specified object is equal to
+        /// this <see cref="StandardPlayingCard"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as StandardPlayingCard);
+        }
+
+        /// <summary>
+        /// Gets a hash code for this <see cref="StandardPlayingCard"/>.
+        /// </summary>
+        /// <returns>
+        /// A a hash code for the current <see cref="StandardPlayingCard"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return Rank.GetHashCode() ^ Suit.GetHashCode();
+        }
+
+        #endregion // End public methods region.
+
+        #endregion // End methods region.
+
+        #region Operators
+
+        /// <summary>
+        /// Determines whether two <see cref="StandardPlayingCard"/>
+        /// instances are equal to each other.
+        /// </summary>
+        /// <param name="card1">
+        /// The <see cref="StandardPlayingCard"/> on the left hand of the
+        /// expression.
+        /// </param>
+        /// <param name="card2">
+        /// The <see cref="StandardPlayingCard"/> on the right hand of the
+        /// expression.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="card1"/> is equal to
+        /// <paramref name="card2"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool operator ==(StandardPlayingCard card1, StandardPlayingCard card2)
+        {
+            if (ReferenceEquals(card1, card2))
+            {
+                return true;
+            }
+
+            if (card1 is null)
+            {
+                return false;
+            }
+
+            if (card2 is null)
+            {
+                return false;
+            }
+
+            return card1.Equals(card2);
+        }
+
+        /// <summary>
+        /// Determines whether two <see cref="StandardPlayingCard"/>
+        /// instances are not equal to each other.
+        /// </summary>
+        /// <param name="card1">
+        /// The <see cref="StandardPlayingCard"/> on the left hand of the
+        /// expression.
+        /// </param>
+        /// <param name="card2">
+        /// The <see cref="StandardPlayingCard"/> on the right hand of the
+        /// expression.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="card1"/> is not equal to
+        /// <paramref name="card2"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool operator !=(StandardPlayingCard card1, StandardPlayingCard card2)
+        {
+            if (ReferenceEquals(card1, card2))
+            {
+                return false;
+            }
+
+            if (card1 is null)
+            {
+                return true;
+            }
+
+            if (card2 is null)
+            {
+                return true;
+            }
+
+            return card1.Equals(card2);
+        }
+
+        #endregion // End operators region.
     }
 }
