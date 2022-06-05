091b06e3344a41d8b9b5bb17987cbc62f155bc92
Xyaneon <christhehorton@gmail.com>
2022-06-04 23:50:38 -0400

    feat: Add SuitSymbols constants class

---
+++ Xyaneon.Games.Cards/StandardPlayingCards/SuitSymbols.cs
@@ -0,0 +1,49 @@
+namespace Xyaneon.Games.Cards.StandardPlayingCards
+{
+    /// <summary>
+    /// Constants for standard playing card suit Unicode symbols.
+    /// </summary>
+    /// <seealso cref="Suit"/>
+    public static class SuitSymbols
+    {
+        /// <summary>
+        /// The Unicode symbol for the black club suit (♣).
+        /// </summary>
+        public const char BlackClubSuit = '\u2663';
+        
+        /// <summary>
+        /// The Unicode symbol for the black diamond suit (♦).
+        /// </summary>
+        public const char BlackDiamondSuit = '\u2666';
+        
+        /// <summary>
+        /// The Unicode symbol for the black heart suit (♥).
+        /// </summary>
+        public const char BlackHeartSuit = '\u2665';
+        
+        /// <summary>
+        /// The Unicode symbol for the black spade suit (♠).
+        /// </summary>
+        public const char BlackSpadeSuit = '\u2660';
+        
+        /// <summary>
+        /// The Unicode symbol for the white club suit (♧).
+        /// </summary>
+        public const char WhiteClubSuit = '\u2667';
+        
+        /// <summary>
+        /// The Unicode symbol for the white diamond suit (♢).
+        /// </summary>
+        public const char WhiteDiamondSuit = '\u2662';
+        
+        /// <summary>
+        /// The Unicode symbol for the white heart suit (♡).
+        /// </summary>
+        public const char WhiteHeartSuit = '\u2661';
+        
+        /// <summary>
+        /// The Unicode symbol for the white spade suit (♤).
+        /// </summary>
+        public const char WhiteSpadeSuit = '\u2664';
+    }
+}
