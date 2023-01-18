7533f987d438e63aae77599340ae6727e89c8e79
Christopher Kyle Horton <christhehorton@gmail.com>
2023-01-18 18:31:45 -0500

    test: improve test coverage of DrawPile (#69)

--- Xyaneon.Games.Cards.Test/DrawPileTests.cs
+++ Xyaneon.Games.Cards.Test/DrawPileTests.cs
@@ -33,6 +33,279 @@ namespace Xyaneon.Games.Cards.Test
         }
 
         [TestMethod]
+        public void DrawPile_CardsConstructor_ShouldRejectNullCardsCollection()
+        {
+            var actualException = Assert.ThrowsException<ArgumentNullException>(() => {
+                _ = new DrawPile<Card>(null);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The collection of cards to create the draw pile from cannot be null.");
+        }
+
+        [TestMethod]
+        public void DrawPile_Draw_ShouldRemoveAndReturnTopCard()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedRemainingCardsList = new IntCard[] { middleCard, bottomCard };
+
+            var actualDrawnCard = drawPile.Draw();
+
+            Assert.AreSame(topCard, actualDrawnCard);
+            Assert.That.CardListsAreEqual(expectedRemainingCardsList, drawPile.Cards);
+        }
+
+        [TestMethod]
+        public void DrawPile_Draw_ShouldThrowExceptionWhenEmpty()
+        {
+            var drawPile = new DrawPile<IntCard>();
+
+            var actualException = Assert.ThrowsException<InvalidOperationException>(() => {
+                drawPile.Draw();
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "There are no cards left to draw.");
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawCount_ShouldRemoveAndReturnTopCards()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedDrawnCardsList = new IntCard[] { topCard, middleCard };
+            var expectedRemainingCardsList = new IntCard[] { bottomCard };
+
+            IEnumerable<IntCard> actualDrawnCards = drawPile.Draw(2);
+
+            CollectionAssert.AreEqual(expectedDrawnCardsList, actualDrawnCards.ToList());
+            Assert.That.CardListsAreEqual(expectedRemainingCardsList, drawPile.Cards);
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawCount_ShouldThrowExceptionWhenEmpty()
+        {
+            var drawPile = new DrawPile<IntCard>();
+
+            var actualException = Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                drawPile.Draw(1);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "Too few cards to draw in this draw pile.");
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawCount_ShouldThrowExceptionWhenTooManyCardsDrawn()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            var actualException = Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                drawPile.Draw(4);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "Too few cards to draw in this draw pile.");
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawAtMost_ShouldRemoveAndReturnTopCards()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedDrawnCardsList = new IntCard[] { topCard, middleCard };
+            var expectedRemainingCardsList = new IntCard[] { bottomCard };
+
+            IEnumerable<IntCard> actualDrawnCards = drawPile.DrawAtMost(2);
+
+            CollectionAssert.AreEqual(expectedDrawnCardsList, actualDrawnCards.ToList());
+            Assert.That.CardListsAreEqual(expectedRemainingCardsList, drawPile.Cards);
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawAtMost_ShouldRemoveAndReturnAllCardsIfMoreRequested()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedDrawnCardsList = new IntCard[] { topCard, middleCard, bottomCard };
+            var expectedRemainingCardsList = new IntCard[] { bottomCard };
+
+            IEnumerable<IntCard> actualDrawnCards = drawPile.DrawAtMost(4);
+
+            CollectionAssert.AreEqual(expectedDrawnCardsList, actualDrawnCards.ToList());
+            Assert.That.DrawPileIsEmpty(drawPile);
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawAtMost_ShouldReturnEmptyCollectionForEmptyDrawPile()
+        {
+            var drawPile = new DrawPile<IntCard>();
+
+            IEnumerable<IntCard> actualDrawnCards = drawPile.DrawAtMost(1);
+
+            Assert.That.DrawPileIsEmpty(drawPile);
+            Assert.IsNotNull(actualDrawnCards);
+            Assert.AreEqual(0, actualDrawnCards.Count());
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawAtMost_ShouldReturnEmptyCollectionForZeroRequestedCards()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            IEnumerable<IntCard> actualDrawnCards = drawPile.DrawAtMost(0);
+
+            Assert.That.CardListsAreEqual(cards.Reverse().ToList(), drawPile.Cards);
+            Assert.IsNotNull(actualDrawnCards);
+            Assert.AreEqual(0, actualDrawnCards.Count());
+        }
+
+        [TestMethod]
+        public void DrawPile_DrawAtMost_ShouldThrowExceptionForNegativeRequestedCards()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            var actualException = Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                drawPile.DrawAtMost(-1);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "Non-negative number required.");
+        }
+
+        [TestMethod]
+        public void DrawPile_Insert_ShouldInsertCardAtExpectedIndex()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var insertedCard = new IntCard(4);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedRemainingCardsList = new IntCard[] { topCard, insertedCard, middleCard, bottomCard };
+
+            drawPile.Insert(1, insertedCard);
+
+            Assert.That.CardListsAreEqual(expectedRemainingCardsList.Reverse().ToList(), drawPile.Cards);
+        }
+
+        [TestMethod]
+        public void DrawPile_Insert_ShouldThrowExceptionForNullCard()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            var actualException = Assert.ThrowsException<ArgumentNullException>(() => {
+                drawPile.Insert(1, null);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The card to insert into the draw pile cannot be null.");
+        }
+
+        [TestMethod]
+        public void DrawPile_Insert_ShouldThrowExceptionForNegativeIndex()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+            var insertedCard = new IntCard(4);
+
+            var actualException = Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                drawPile.Insert(-1, insertedCard);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The position to insert the card into the draw pile at cannot be less than zero.");
+        }
+
+        [TestMethod]
+        public void DrawPile_Insert_ShouldThrowExceptionForIndexAboveUpperBound()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+            var insertedCard = new IntCard(4);
+
+            var actualException = Assert.ThrowsException<ArgumentOutOfRangeException>(() => {
+                drawPile.Insert(4, insertedCard);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The position to insert the card into the draw pile at cannot be greater than the number of cards in the draw pile.");
+        }
+
+        [TestMethod]
+        public void DrawPile_PlaceAtBottomTest()
+        {
+            var expectedCards = new IntCard[]
+            {
+                // 3 (new top), 2, 1, 4 (new bottom)
+                new IntCard(3), new IntCard(2), new IntCard(1), new IntCard(4)
+            };
+            var cards = new IntCard[]
+            {
+                // 1 (old bottom), 2, 3, 4 (old top)
+                new IntCard(1), new IntCard(2), new IntCard(3), new IntCard(4)
+            };
+            var drawPile = new DrawPile<IntCard>(cards);
+            List<IntCard> actualCards;
+
+            var drawnCard = drawPile.Draw();
+            drawPile.PlaceAtBottom(drawnCard); // place card with value 4 at the bottom
+            actualCards = new List<IntCard>(drawPile.Cards);
+
+            // Use collections assertions for lists because HashSet is unable to compare different sets.
+            // Otherwise, GetHashCode and Equals methods would have to be tamed.
+            CollectionAssert.AreEqual(expectedCards, actualCards, Comparer<IntCard>.Create((a, b) =>
+            {
+                return a.Value - b.Value;
+            }));
+        }
+
+        [TestMethod]
+        public void DrawPile_PlaceAtBottom_ShouldThrowExceptionForNullCard()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            var actualException = Assert.ThrowsException<ArgumentNullException>(() => {
+                drawPile.PlaceAtBottom(null);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The card to place at the bottom of the draw pile cannot be null.");
+        }
+
+        [TestMethod]
+        public void DrawPile_PlaceOnTop_ShouldPlaceCardOnTop()
+        {
+            var topCard = new IntCard(1);
+            var middleCard = new IntCard(2);
+            var bottomCard = new IntCard(3);
+            var insertedCard = new IntCard(4);
+            var drawPile = new DrawPile<IntCard>(new IntCard[] { bottomCard, middleCard, topCard });
+            var expectedCardsList = new IntCard[] { insertedCard, topCard, middleCard, bottomCard };
+
+            drawPile.PlaceOnTop(insertedCard);
+
+            Assert.That.CardListsAreEqual(expectedCardsList, drawPile.Cards);
+        }
+
+        [TestMethod]
+        public void DrawPile_PlaceOnTop_ShouldThrowExceptionForNullCard()
+        {
+            var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
+            var drawPile = new DrawPile<IntCard>(cards);
+
+            var actualException = Assert.ThrowsException<ArgumentNullException>(() => {
+                drawPile.PlaceOnTop(null);
+            });
+
+            Assert.That.ExceptionMessageStartsWith(actualException, "The card to place on top of the draw pile cannot be null.");
+        }
+
+        [TestMethod]
         public void DrawPile_BasicShuffleTest()
         {
             var cards = new IntCard[] { new IntCard(1), new IntCard(2), new IntCard(3) };
@@ -225,7 +498,7 @@ namespace Xyaneon.Games.Cards.Test
 
             ShuffleFunction<IntCard> shuffleAlgorithm = cards => cards.Reverse().ToList();
             var expectedCardList = cards2.Reverse().Concat(cards1).Reverse().ToList();
-            
+
             drawPile.ShuffleIn(cards2, shuffleAlgorithm);
             var actualCardList = drawPile.Cards;
 
@@ -248,34 +521,6 @@ namespace Xyaneon.Games.Cards.Test
             Assert.That.CardSetsAreEqual(expectedCardSet, actualCardSet);
             Assert.That.CardSetsAreEqual(expectedCardSet, new HashSet<IntCard>(drawnCards));
             Assert.That.DrawPileIsEmpty(drawPile);
-        }
-
-        [TestMethod]
-        public void DrawPile_PlaceAtBottomTest()
-        {
-            var expectedCards = new IntCard[]
-            {
-                // 3 (new top), 2, 1, 4 (new bottom)
-                new IntCard(3), new IntCard(2), new IntCard(1), new IntCard(4)
-            };
-            var cards = new IntCard[]
-            {
-                // 1 (old bottom), 2, 3, 4 (old top)
-                new IntCard(1), new IntCard(2), new IntCard(3), new IntCard(4)
-            };
-            var drawPile = new DrawPile<IntCard>(cards);
-            List<IntCard> actualCards;
-
-            var drawnCard = drawPile.Draw();
-            drawPile.PlaceAtBottom(drawnCard); // place card with value 4 at the bottom
-            actualCards = new List<IntCard>(drawPile.Cards);
-
-            // Use collections assertions for lists because HashSet is unable to compare different sets.
-            // Otherwise, GetHashCode and Equals methods would have to be tamed.
-            CollectionAssert.AreEqual(expectedCards, actualCards, Comparer<IntCard>.Create((a, b) =>
-            {
-                return a.Value - b.Value;
-            }));
         }
     }
 }
